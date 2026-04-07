af8996c6558df782605777d70facaeee83b38fc3
Sam <CSC308SamB>
2026-04-06 23:48:43 -0400

    Transitions

---
+++ ExampleController.swift
@@ -0,0 +1,29 @@
+//
+//  ExampleController.swift
+//  GroupProposal2
+//
+//  Created by Boston, Samuel T. on 4/6/26.
+//
+
+import UIKit
+
+class ExampleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ExampleView.swift
@@ -0,0 +1,8 @@
+//
+//  ExampleView.swift
+//  GroupProposal2
+//
+//  Created by Boston, Samuel T. on 4/6/26.
+//
+
+import Foundation
